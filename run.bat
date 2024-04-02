ce5691850ea178b7596bf313e055ef85bd4676fb
Silas Francis <123188381+silasfrancis@users.noreply.github.com>
2024-04-02 04:05:29 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
-rem pytest -s -v -m "sanity or regression" --html=./Reports/report.html testCases/ --browser chrome
-pytest -s -v -m "sanity and regression" --html=./Reports/report.html testCases --browser chrome
-pytest -s -v -m "regression" --html=./Reports/report.html testCases --browser chrome
+pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ 
+rem pytest -s -v -m "sanity or regression" --html=./Reports/report.html testCases/ 
+pytest -s -v -m "sanity and regression" --html=./Reports/report.html testCases 
+pytest -s -v -m "regression" --html=./Reports/report.html testCases 
